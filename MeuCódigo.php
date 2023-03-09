2b5467986191a190135c59084ea629919e611063
vi7neto <127431493+vi7neto@users.noreply.github.com>
2023-03-09 16:24:24 -0300

    Add files via upload

---
+++ MeuCódigo.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/estilo.css">
+    <title>Meu Código PHP</title>
+</head>
+
+<body>
+    <div>
+        <?php
+        
+/* $num = 4;
+$nome = 'Júlia';
+
+echo $nome;
+
+
+$num = 4;
+$nome = 'Júlia';
+$num = 4.5;
+
+echo $num;
+
+echo $num vai gerar de resultado a última atribuição da variável que foi 4.5 (que irá aparecer no navegador) e não o número 4 da primeira atribuição. */
+
+
+
+/* $idade=29;
+$Julia='A Júlia tem 24 anos';
+echo $Julia.' e o Fernando tem '.$idade.' anos.';
+
+Acima temos um exemplo de Concatenação, unimos o conteúdo de duas
+strings, a idade de Julia e a idade de Fernando. Utilizamos (.)
+pontinhos (.) para gerar a ligação da Concatenação. */
+
+
+/* Operadores aritméticos:
+
+$x = 5;
+$y = 2;
+echo $x + $y; */
+
+
+/* $a=10;
+$b=5;
+$c=$a*$b;
+echo"<h3>O resultado da multiplicação é: $c</h3>"
+
+Operadores aritméticos usados em PHP:
+(-) Subtração
+(/) Divisão
+(*) Multiplicação
+(%) Módulo (Resto)
+(**) Exponenciação */
+
+
+/* Operadores de Comparação no PHP:
+(==) Igual
+(===) Identico
+(!=) Não Igual
+(<>) Não Igual
+(!==) Não Identico
+(>) Maior que
+(<) Menor que
+(>=) Maior ou igual a
+(<=) Menor ou igual a
+(<=>) Nave espacial
+
+(<=>) Nave espacial faz uma comparação simultânea, e pode apresentar um dos três resultados abaixo:
+Se o primeiro número comparado ao segundo for menor, o resultado será int(-1); se o primeiro número comparado ao segundo for maior, o resultado será int(1); se o primeiro número comparado ao segundo for igual, o resultado será int(0). */
+
+/* $a = 10;
+$b = 5;
+$c = 2;
+$d = 2;
+
+echo 'O resultado de '.$a. ' com '.$b.' para saber se são iguais, é: <br>';
+var_dump($a == $b);
+echo '</br></br>';
+//Vai dar bool(false).
+
+echo 'O resultado da comparação de '. $c . ' com '.$d.' para saber se são iguais, é: <br/>';
+var_dump($c == $d);
+//Vai dar bool(true).
+
+//UTILIZAMOS A TAG var_dump em COMPARAÇÕES. */
+
+
+        ?>
+    </div>
+</body>
+
+</html>
